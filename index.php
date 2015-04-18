4ffa265d2327ccc217a0b2153f6bb8f38e0dc234
Frederico Augusto Willig <fredewillig@gmail.com>
2015-04-17 21:32:26 -0300

    Imagens e posições
    Adição do logo do projeto.

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 	<div class="row clearfix">
 		
 		<div class="col-md-12 column" align="center">
-			<img class="img-circle" alt="400x400" src="http://lorempixel.com/400/400/">
+			<img src="img/logo_400.png"> <!-- class="img-circle" alt="600x600" -->
 			<h1 class="text-center">
 				LOGICANDO
 			</h1>
@@ -70,24 +70,24 @@
 				</ol>
 				<div class="carousel-inner">
 					<div class="item active">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/1">
+						<img alt="" src="img/carrossel_1.png">
 						<div class="carousel-caption">
 							<h4>
-								First Thumbnail label
+								Logicando - Faça parte dessa ideia
 							</h4>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								O projeto LOGICANDO tem o objetivo de incentivar o desenvolvimento do raciocínio lógico através de jogos virtuais e acompanhamento do desenvolvimento de atividades em sala de aula.
 							</p>
 						</div>
 					</div>
 					<div class="item">
-						<img alt="" src="http://lorempixel.com/1600/500/sports/2">
+						<img alt="" src="img/carrossel_2.png">
 						<div class="carousel-caption">
 							<h4>
-								Second Thumbnail label
+								Logicando - Faça parte dessa ideia
 							</h4>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								O projeto LOGICANDO tem o objetivo de incentivar o desenvolvimento do raciocínio lógico através de jogos virtuais e acompanhamento do desenvolvimento de atividades em sala de aula.
 							</p>
 						</div>
 					</div>
@@ -172,7 +172,8 @@
 			</p>
 		</div>
 	</div>
-    <div id="div_cadastro_login">
+	
+	<div id="div_cadastro_login">
         <?php
             include './login_cadastro.php';
         ?>
