d68f44fdaca81dcab5f0dcf5cb830d1b02c34a21
webjhon <colorado.santos@gmail.com>
2015-04-19 02:07:10 -0300

    Alinhamento botão "NÃO POSSUO CADASTRO"
    Consegui achar uma maneira do botão "NÃO POSSUO CADASTRO" continuar
    dentro do formulário e não fazer validação dos campos.
    
    Ele agora não é mais um BUTTON, e sim um <a> com as classes "btn
    btn-primary", portanto o mesmo não intefere no funcionamento do
    formulário.

--- login_cadastro.php
+++ login_cadastro.php
@@ -1,94 +1,95 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="utf-8">
-  <title>Cadastro e login</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
-	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
-	<!--script src="js/less-1.3.3.min.js"></script-->
-	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	<head>
+		<meta charset="utf-8">
+		<title>Cadastro e login</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		
+		<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+		<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+		<!--script src="js/less-1.3.3.min.js"></script-->
+		<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+		
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+			<script src="js/html5shiv.js"></script>
+		<![endif]-->
+		
+		<!-- Fav and touch icons -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+		<link rel="shortcut icon" href="img/favicon.png">
+		
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/scripts.js"></script>
+	</head>
 	
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-
-  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-  <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-  <![endif]-->
-
-  <!-- Fav and touch icons -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
-  <link rel="shortcut icon" href="img/favicon.png">
-  
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/scripts.js"></script>
-</head>
-
-<body>
-
-
-<div id="form_login">
-<form class="form-horizontal">
-<fieldset>
-
-<!-- Form Name -->
-<legend>Já tem cadastro ?</legend>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="cad_login">Login</label>  
-  <div class="col-md-5">
-  <input id="cad_login" name="cad_login" placeholder="nome de login" class="form-control input-md" required="" type="text">
-  <span class="help-block">Insira um nome de usuário</span>  
-  </div>
-</div>
-
-<!-- Password input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="cad_passwd">Insira uma senha</label>
-  <div class="col-md-5">
-    <input id="cad_passwd" name="cad_passwd" placeholder="Senha" class="form-control input-md" required="" type="password">
-    <span class="help-block">Informe uma senha (8 caracteres)</span>
-  </div>
-</div>
-
-<!-- Button Entrar-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="login_envia"></label>
-  <div class="col-md-4">
-    <button id="login_envia" name="login_envia" class="btn btn-primary">Entrar</button>
-	
-  </div>
-</div>
-
-
-
-
-<!-- Button Não tenho cadastro azul-->
-<!--<div class="form-group">
-  <label class="col-md-4 control-label" for="botao_cadastro"></label>
-  <div class="col-md-4">
-    <button id="botao_cadastro" type="submit" name="botao_cadastro" class="btn btn-primary">Não possuo cadastro</button>
-  </div>
-</div>
--->
-
-
-
-
-</fieldset>
-</form>
-    <button id="botao_cadastro" type="submit" name="botao_cadastro" class="btn btn-primary">Não possuo cadastro</button>
-</div>
-
-
-</body>
-</html>
+	<body>
+		
+		
+		<div id="form_login">
+			<form class="form-horizontal">
+				<fieldset>
+					
+					<!-- Form Name -->
+					<legend>Já tem cadastro ?</legend>
+					
+					<!-- Text input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="cad_login">Login</label>  
+						<div class="col-md-5">
+							<input id="cad_login" name="cad_login" placeholder="nome de login" class="form-control input-md" required="" type="text">
+							<span class="help-block">Insira um nome de usuário</span>  
+						</div>
+					</div>
+					
+					<!-- Password input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="cad_passwd">Insira uma senha</label>
+						<div class="col-md-5">
+							<input id="cad_passwd" name="cad_passwd" placeholder="Senha" class="form-control input-md" required="" type="password">
+							<span class="help-block">Informe uma senha (8 caracteres)</span>
+						</div>
+						</div>
+						
+						<!-- Button Entrar-->
+						<div class="form-group">
+						<label class="col-md-4 control-label" for="login_envia"></label>
+						<div class="col-md-4">
+						<button id="login_envia" name="login_envia" class="btn btn-primary">Entrar</button>
+						<a id="botao_cadastro" class="btn btn-primary">Não possuo cadastro</a>
+						
+						</div>
+						</div>
+						
+						
+						
+						
+						<!-- Button Não tenho cadastro azul-->
+						<!--<div class="form-group">
+						<label class="col-md-4 control-label" for="botao_cadastro"></label>
+						<div class="col-md-4">
+						<button id="botao_cadastro" type="submit" name="botao_cadastro" class="btn btn-primary">Não possuo cadastro</button>
+						</div>
+						</div>
+						-->
+						
+						
+						
+						
+						</fieldset>
+						</form>
+						
+						</div>
+						
+						
+	</body>
+</html>						
